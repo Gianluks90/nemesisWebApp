9f8af2ebca554eabb7a90020fd65ed543cf7ccb1
andreabocchimpani <andrea.bocchimpani@ettsolutions.it>
2022-10-24 00:26:33 +0200

    Classic selection added
    - Classic Mode method
    - Fixed css problems
    - To do scroll height team component

--- src/app/components/team/team.component.ts
+++ src/app/components/team/team.component.ts
@@ -9,18 +9,14 @@ export class TeamComponent implements OnInit {
 
   @Input()
   set teamInfo(data: any) {
-    // this.incipit = data.type === 'random'? "Random players number" : "Manual players number";
     this.incipit = "Select number of players";
     this.type = data.type;
-    console.log('datatype', this.type)
-    this.random = data.type === 'random'? true : false;
     this.filteredCharacters = data.characters;
   }
 
   @Output() closeEmitted = new EventEmitter<boolean>();
 
   public incipit = '';
-  public random: boolean = false;
   public squad: any[] = [];
   public filteredCharacters: any[];
   public manualNumberClicked: boolean = false;
@@ -29,11 +25,15 @@ export class TeamComponent implements OnInit {
   public manualSquadCompleted: boolean = false;
   public firstCharacterSelected: boolean = false;
   public type = '';
+  public colorArray = [];
+  public choosedColor = [];
+  public classicTeam1 = [];
+  public classicTeam2 = [];
+  public height: any;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   closeComponent() {
     this.closeEmitted.emit(true);
@@ -71,38 +71,6 @@ export class TeamComponent implements OnInit {
       }
   }
 
-  // getMoreRandom(arr, n) {
-  //   console.log('getMoreRandom', arr, n);
-
-  //   const tempArray = [...arr];
-  //   this.squad = [];
-  //   while (this.squad.length !== n) {
-  //     const index = Math.floor(Math.random() * tempArray.length);
-  //     const selectedCharacter = tempArray[index];
-  //     if (this.squad.filter(c => c.color.includes(selectedCharacter.color[0])).length === 0) {
-  //       this.squad.push(tempArray[index]);
-  //       tempArray.splice(index, 1);
-  //     }
-  //   }
-  // }
-
-  // manualNumberPlayerSelected(arr, n){
-  //   this.manualNumberClicked = true;
-  //   const tempArray = [...arr];
-  //   this.squadToChoice = [];
-  //   if (this.squad.length === 0) {
-  //     this.teamCounter = n;
-  //   }
-  //     while (this.squadToChoice.length !== 2) {
-  //       const index = Math.floor(Math.random() * tempArray.length);
-  //       const selectedCharacter = tempArray[index];
-  //       if (this.squadToChoice.filter(c => c.color.includes(selectedCharacter.color[0])).length === 0) {
-  //         this.squadToChoice.push(tempArray[index]);
-  //         tempArray.splice(index, 1);
-  //       }
-  //     }
-  // }
-
   characterSelected(character: any){
     this.firstCharacterSelected = true;
     if (this.teamCounter > 0) {
@@ -111,6 +79,51 @@ export class TeamComponent implements OnInit {
       if (this.teamCounter > 0) {
         this.filteredCharacters = this.filteredCharacters.filter(c => !c.color.includes(character.color));
         this.manualNumberPlayerSelected(this.filteredCharacters, this.teamCounter);
+      } else {
+        this.manualSquadCompleted = true;
+        this.firstCharacterSelected = false;
+      }
+    }
+  }
+
+  classicMode(arr, n){
+    this.manualNumberClicked = true;
+    const tempArray = [...arr];
+    let tempColorArray = [];
+    tempArray.forEach(char => tempColorArray.push(char.color));
+    this.colorArray = [...new Set(tempColorArray)];
+    if (this.squad.length === 0) {
+      this.teamCounter = n;
+    }
+    this.choosedColor = [];
+    this.classicTeam1 = [];
+    this.classicTeam2 = [];
+    while (this.choosedColor.length !==2) {
+      const index = Math.floor(Math.random() * this.colorArray.length);
+      const selectedColor = this.colorArray[index];
+      if (!this.choosedColor.some(color => color === selectedColor)) {
+        this.choosedColor.push(this.colorArray[index]);
+      }
+    }
+    tempArray.forEach(char => {
+      if (char.color === this.choosedColor[0]) {
+        this.classicTeam1.push(char);
+      }
+      if (char.color === this.choosedColor[1]) {
+        this.classicTeam2.push(char);
+      }
+    });
+  }
+
+  characterSelectedClassic(character: any){
+    this.firstCharacterSelected = true;
+    if (this.teamCounter > 0) {
+      this.squad.push(character);
+      this.teamCounter--;
+      if (this.teamCounter > 0) {
+        this.filteredCharacters = this.filteredCharacters.filter(c => !c.color.includes(character.color));
+        this.colorArray = this.colorArray.filter(c => !c.includes(character.color));
+        this.classicMode(this.filteredCharacters, this.teamCounter);
       } else {
         this.manualSquadCompleted = true;
         this.firstCharacterSelected = false;
